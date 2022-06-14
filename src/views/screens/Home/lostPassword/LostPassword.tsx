3b824aa1b2fe754df4c9c7259dec9c57905fd216
elizabeth <elizabethcubides200616@gmail.com>
2022-06-14 14:46:47 -0500

    cotraseña

--- src/views/screens/Home/lostPassword/LostPassword.tsx
+++ src/views/screens/Home/lostPassword/LostPassword.tsx
@@ -25,7 +25,7 @@ const LostPassword: FC<ILostPasswordProps> = ({
             className="modal-lost-password"
         >
             <div className="w-full h-full flex justify-center items-center">
-                <StyledCard background={require("assets/images/modal-image-module.png")}>
+                <StyledCard background={require("assets/images/modal-image-module1.png")}>
                     <CardContent>
                         <Grid item md={12}>
                             <StyledTitleHeader className="flex justify-center">
